82e08c47ccf22f375ef74114d9d19545d23a8eeb
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2023-09-09 14:01:55 -0500

    [Add] delete repo method

--- src/Repository.ts
+++ src/Repository.ts
@@ -1,8 +1,24 @@
 import { AggregateRoot, IIdentifier } from "@juandardilag/value-objects";
 
-export interface Repository<T extends AggregateRoot> {
-  persist(aggregateRoot: T): Promise<void>;
-  updateOne(aggregateRoot: T): Promise<void>;
+export interface Repository<T extends AggregateRoot>
+  extends CreatorRepository<T>,
+    ReaderRepository<T>,
+    UpdaterRepository<T>,
+    DeleterRepository<T> {}
+
+export interface CreatorRepository<T extends AggregateRoot> {
+  persist(_: T): Promise<void>;
+}
+
+export interface ReaderRepository<T extends AggregateRoot> {
   getAll(): Promise<T[]>;
   getByID(id: IIdentifier<string | number>): Promise<T>;
+}
+
+export interface UpdaterRepository<T extends AggregateRoot> {
+  updateOne(_: T): Promise<void>;
+}
+
+export interface DeleterRepository<T extends AggregateRoot> {
+  deleteOne(_: T): Promise<void>;
 }

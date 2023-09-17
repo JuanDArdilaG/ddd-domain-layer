c33970e3f68452cbd5192ab9e217d2b3121a5e2e
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2023-09-17 18:15:32 -0500

    [Fix] repo in storableAR

--- src/StorableAggregateRoot.ts
+++ src/StorableAggregateRoot.ts
@@ -7,24 +7,8 @@ import { Repository } from "./Repository/Repository";
 export abstract class StorableAggregateRoot extends AggregateRoot {
   constructor(
     _id: IdentifierValueObject<string>,
-    private _repo: Repository<AggregateRoot>
+    readonly repo: Repository<AggregateRoot>
   ) {
     super(_id);
-  }
-
-  async persist(): Promise<void> {
-    await this._repo.persist(this);
-  }
-
-  async update(): Promise<void> {
-    await this._repo.updateOne(this);
-  }
-
-  async getAll(): Promise<AggregateRoot[]> {
-    return await this._repo.getAll();
-  }
-
-  async get(): Promise<AggregateRoot> {
-    return await this._repo.getByID(this._id);
   }
 }
